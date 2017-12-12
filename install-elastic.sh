4e83f572261a5c2f9aa8d977c49abbb7f0ee6331
Daniela Ruz <druzmieres@users.noreply.github.com>
2017-12-12 16:20:02 -0300

    asks for ip, port, ram amount

--- install-elastic.sh
+++ install-elastic.sh
@@ -1,60 +1,93 @@
 #!/bin/bash
 
-#Install requirements
+# Install requirements
 ./requirements
 
-### Download and install Elasticsearch
-### Check http://www.elasticsearch.org/download/ for latest version of Elasticsearch and replace wget link
-#Only download the file if it doesn't already exist
+## Download and install Elasticsearch
+## Check http://www.elasticsearch.org/download/ for latest version of Elasticsearch and replace wget link
+# Only download the file if it does not already exist
 if [ ! -f elasticsearch-6.0.0.deb ]; then
     wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0.deb
 fi
-
-#Dpkg deb
+# Install deb
 sudo dpkg -i elasticsearch-6.0.0.deb
 
-# Enable Elastic to start automatically (on bootup)
+# Enable Elastic to start on bootup
 sudo update-rc.d elasticsearch defaults 95 10
 
-# set JAVA_HOME
+# Set JAVA_HOME (in case it is not automatically set)
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
-_repeat="Y"
-
-while [ $_repeat = "Y" ]
+# Ask user for path to store data
+_repeatPath="Y"
+while [ $_repeatPath = "Y" ]
 do
-	echo -n "Enter path to data directory (leave blank for default): " #(separate multiple locations by comma)
+	echo "Enter path to data directory"
+	echo -n "separate multiple locations by comma (leave blank for default): "
 read DPATH
+# If empty, then set the default path
 if [ -z "$DPATH" ]; then
-    sudo sed -i 's#^path.data: .*$#path.data: /var/lib/elasticsearch#' /etc/elasticsearch/elasticsearch.yml
+    sudo sed -i 's|^path.data: .*$|path.data: /var/lib/elasticsearch|' /etc/elasticsearch/elasticsearch.yml
     echo "Set to the default path"
-    _repeat="N"
+    _repeatPath="N"
+# Else, check if the path exists; if it doesn't ask again
 elif [ ! -e "$DPATH" ]; then
     echo "Path not valid"
-    #ask for path again
 else
-    #Give permissions to elasticsearch
+    # Give permissions to elasticsearch (this is the default user running the application)
     setfacl -m u:elasticsearch:rwx $DPATH
-    #replace path
-    sudo sed -i 's#^path.data: .*$#path.data: '"$DPATH"'#' /etc/elasticsearch/elasticsearch.yml 
-    _repeat="N"
+    # Replace the old path with the given one
+    sudo sed -i 's|^path.data: .*$|path.data: '"$DPATH"'|' /etc/elasticsearch/elasticsearch.yml 
+    _repeatPath="N"
 fi
 done
 
-#Set IP and Port
-sudo sed -i 's|^#network.host: .*$|network.host: 172.17.74.243|' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's|^#http.port: .*$|http.port: 9200|' /etc/elasticsearch/elasticsearch.yml
+# Ask user to set IP
+_repeatIP="Y"
+while [ $_repeatIP = "Y" ]
+do
+	echo -n "Enter IP address: "
+read IP
+# If empty
+if [ -z "$IP" ]; then
+    : # Do nothing
+else
+    # Replace IP
+    sudo sed -i 's|^#network.host: .*$|network.host: '"$IP"'|' /etc/elasticsearch/elasticsearch.yml 
+    _repeatIP="N"
+fi
+done
 
-#RAM setting. Minimum and maximum heap size should be the same.
+# Ask user to set port
+_repeatPort="Y"
+while [ $_repeatPort = "Y" ]
+do
+	echo -n "Enter port number: "
+read PORT
+# If empty
+if [ -z "$PORT" ]; then
+    : # Do nothing
+else
+    # Replace port
+    sudo sed -i 's|^#http.port: .*$|http.port: '"$PORT"'|' /etc/elasticsearch/elasticsearch.yml 
+    _repeatPort="N"
+fi
+done
+
+# Ask user to set amount of RAM
+# RAM setting. Minimum and maximum heap size should be the same.
 #Don't set more than 50% of available RAM. Don't exceed 32 GB as it'll be counterproductive.
-sudo sed -i 's|^#-Xms.*$|-Xms14g|' /etc/elasticsearch/jvm.options
-sudo sed -i 's|^#-Xmx.*$|-Xms14g|' /etc/elasticsearch/jvm.options
+echo "RAM setting. Do not set more than 50% of available RAM. Do not exceed 32 GB. Less than 8 GB tends to be counterproductive."
+echo -n "Enter RAM amount in GB: "
+read RAM
+sudo sed -i 's|^-Xms.*$|-Xms'"$RAM"'g|' /etc/elasticsearch/jvm.options
+sudo sed -i 's|^-Xmx.*$|-Xms'"$RAM"'g|' /etc/elasticsearch/jvm.options
 
 ### Start Elasticsearch
 sudo /etc/init.d/elasticsearch start
 
 ### Make sure service is running
-curl http://localhost:9200
+curl http://$IP:9200
 
 ### Should return something like this:
 # {
@@ -69,3 +102,5 @@ curl http://localhost:9200
 #  },
 #  "tagline" : "You Know, for Search"
 #}
+
+#Download, install and run cerebro
