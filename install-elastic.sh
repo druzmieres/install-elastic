c2d1dce6be72d96d93cdd237431bd40583dc4feb
Daniela Ruz <druzmieres@users.noreply.github.com>
2017-12-12 10:49:26 -0300

    added requirements script

--- install-elastic.sh
+++ install-elastic.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 ### Install Java 8 and agree to the license
-cd ~
+#cd ~
 # Requirements
-sudo apt -y install aptitude
-sudo apt -y install curl
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt update
-sudo apt -y install aptitude
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo aptitude -y install oracle-java8-installer
+#sudo apt -y install aptitude
+#sudo apt -y install curl
+#sudo add-apt-repository -y ppa:webupd8team/java
+#sudo apt update
+#sudo apt -y install aptitude
+#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+#sudo aptitude -y install oracle-java8-installer
+
+#Run requirements
+./requirements
 
 ### Download and install Elasticsearch
 ### Check http://www.elasticsearch.org/download/ for latest version of Elasticsearch and replace wget link
